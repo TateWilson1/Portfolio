63b75d75d76b6bd8320eccca96c17dc627e47816
Tate Wilson <tate.wilson2022@gmail.com>
2024-10-28 17:00:35 -0400

    general maintence

--- javascript/intro.js
+++ javascript/intro.js
@@ -3,6 +3,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Wait 2.5 seconds before redirecting to index.html
     setTimeout(() => {
-        window.location.href = "index.html"; // Redirect to your main page
-    }, 2500); // Adjust timing as needed
+        window.location.href = "index.html"; 
+    }, 2500); 
 });
--- javascript/script.js
+++ javascript/script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
     contentSections.forEach((section, index) => {
         setTimeout(() => {
             section.classList.add('visible');
-        }, index * 700); // Adjust timing as needed
+        }, index * 700); 
     });
 });
 
@@ -18,9 +18,9 @@ function type() {
     if (currentCharIndex < phrases[currentPhraseIndex].length) {
         dynamicTextElement.textContent += phrases[currentPhraseIndex].charAt(currentCharIndex);
         currentCharIndex++;
-        setTimeout(type, 100); // Adjust typing speed here
+        setTimeout(type, 100); 
     } else {
-        setTimeout(erase, 1000); // Pause before erasing
+        setTimeout(erase, 1000); 
     }
 }
 
@@ -28,13 +28,13 @@ function erase() {
     if (currentCharIndex > 0) {
         dynamicTextElement.textContent = phrases[currentPhraseIndex].substring(0, currentCharIndex - 1);
         currentCharIndex--;
-        setTimeout(erase, 50); // Adjust erasing speed here
+        setTimeout(erase, 50); 
     } else {
-        currentPhraseIndex = (currentPhraseIndex + 1) % phrases.length; // Move to the next phrase
-        setTimeout(type, 500); // Pause before typing the next phrase
+        currentPhraseIndex = (currentPhraseIndex + 1) % phrases.length; 
+        setTimeout(type, 500); 
     }
 }
 
-// Start the typewriter effect with a delay
-setTimeout(type, 2000); // Delay before the typing starts (2000 milliseconds = 2 seconds)
+
+setTimeout(type, 2000); 
 
